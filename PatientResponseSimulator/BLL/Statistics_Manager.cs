a35bdad7a5312d99e367dbcb9365e9cb0a8c4ab3
abayeh <abayeh@gmail.com>
2013-07-25 17:10:49 -0400

    Initial commit of Statistics and Output managers

---
+++ PatientResponseSimulator/BLL/Statistics_Manager.cs
@@ -0,0 +1,172 @@
+﻿//------------------------------------------------------------------------------------------
+// <copyright file="Statistics_Manager.cs" company="Tessella">
+//     Copyright © Tessella 2013. All rights reserved.
+// </copyright>
+// <project>Tessella C# training exercise, Assay Scheduler</project>
+// <summary></summary>
+// <svn>
+//  <lastChanged>$Date$</lastChanged>
+//  <by>BAYA</by>
+//  <version>$Revision$</version>
+//  <source>$HeadURL$</source>
+// </svn>
+//------------------------------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PatientResponseSimulator
+{
+    /// <summary>
+    /// Definition of the Statistics_Manager class.
+    /// </summary>
+    public class Statistics_Manager
+    {
+        #region Fields
+
+        /// <summary>
+        /// Private instance of the Statistics_Manager class.
+        /// This is used to ensure that there is only once
+        /// instance, and that the Statistic_Manager is a
+        /// Singleton.
+        /// </summary>
+        private static Statistics_Manager instance;
+
+        /// <summary>
+        /// This is the List that will store the statistic type
+        /// for each endpoint that we are simulating.
+        /// </summary>
+        private List<StatisticType> endpointStatisticTypes;
+
+        /// <summary>
+        /// This is the List that will store the history dependence
+        /// requirement for each endpoint simulated.
+        /// </summary>
+        private List<bool> historyDependencies;
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Prevents a default instance of the Statistics_Manager class from being created.
+        /// </summary>
+        private Statistics_Manager()
+        {
+            endpointStatisticTypes = new List<StatisticType>();
+
+            historyDependencies = new List<bool>();
+        }
+
+        #endregion
+
+        #region Enums
+
+        /// <summary>
+        /// This enum lists the different types of statistical
+        /// simulations that can be run.
+        /// </summary>
+        public enum StatisticType
+        {
+            /// <summary>
+            /// Normal Distribution type
+            /// </summary>
+            Normal,
+
+            /// <summary>
+            /// Exponential Rise type
+            /// </summary>
+            ExponentialRise,
+
+            /// <summary>
+            /// Exponential Decay type
+            /// </summary>
+            ExponentialDecay
+        }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets the single instance of the Statistics Manager.
+        /// </summary>
+        public static Statistics_Manager Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Statistics_Manager();
+                }
+
+                return instance;
+            }
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Method to add an endpoint to the statistics manager
+        /// </summary>
+        /// <param name="endpointStatisticType">
+        /// Allowed Values are Normal, ExponentialRise, and ExponentialDecay
+        /// </param>
+        /// <param name="historyDependence">
+        /// true is there is a history dependence.
+        /// </param>
+        public void AddEndPoint(StatisticType endpointStatisticType, bool historyDependence)
+        {
+            endpointStatisticTypes.Add(endpointStatisticType);
+            historyDependencies.Add(historyDependence);
+            return;
+        }
+
+        /// <summary>
+        /// Box-Mueller random number generator that yields a specified 
+        /// mean and standard deviation. The Box-Mueller scheme implemented
+        /// is the polar method.
+        /// </summary>
+        /// <param name="mean">
+        /// Mean of the normal distribution created.
+        /// </param>
+        /// <param name="standardDeviation">
+        /// Standard deviation of the distribution created.
+        /// </param>
+        /// <param name="distributionSize">
+        /// The size of the distribution created.
+        /// </param>
+        /// <returns>
+        /// Returns the desired normal distribution.
+        /// </returns>
+        private List<double> NormalDistribution(double mean, double standardDeviation, int distributionSize)
+        {
+            // Seed for random number generation
+            Random r = new Random();
+
+            List<double> normalDistribution = new List<double>();
+
+            double u, v, s;
+
+            for (int i = 0; i < distributionSize; i++)
+            {
+                do
+                {
+                    u = (2.0 * r.NextDouble()) - 1.0;
+                    v = (2.0 * r.NextDouble()) - 1.0;
+                    s = (u * u) + (v * v);
+                }
+                while (s >= 1.0);
+
+                normalDistribution.Add(((u * Math.Sqrt(-2.0 * Math.Log(s) / s)) * standardDeviation) + mean);
+            }
+
+            return normalDistribution;
+        }
+
+        #endregion
+    }
+}
---
+++ PatientResponseSimulator/DAL/Output_Manager.cs
@@ -0,0 +1,105 @@
+﻿//------------------------------------------------------------------------------------------
+// <copyright file="Output_Manager.cs" company="Tessella">
+//     Copyright © Tessella 2013. All rights reserved.
+// </copyright>
+// <project>Tessella C# training exercise, Assay Scheduler</project>
+// <summary></summary>
+// <svn>
+//  <lastChanged>$Date$</lastChanged>
+//  <by>BAYA</by>
+//  <version>$Revision$</version>
+//  <source>$HeadURL$</source>
+// </svn>
+//------------------------------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PatientResponseSimulator.BLL
+{
+    /// <summary>
+    /// Definition of the Output_Manager class.
+    /// </summary>
+    public class Output_Manager
+    {
+        #region Fields
+
+        /// <summary>
+        /// Singleton instance of the Output_Manager class.
+        /// </summary>
+        private static Output_Manager instance;
+
+        /// <summary>
+        /// String used for formatting the output
+        /// </summary>
+        private static string outputString;
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Prevents a default instance of the Output_Manager class from being created.
+        /// </summary>
+        private Output_Manager()
+        {
+            outputString = string.Empty;
+        }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets the singleton instance of the Output_Manager.
+        /// </summary>
+        public static Output_Manager Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Output_Manager();
+                }
+
+                return instance;
+            }
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Sets the output string
+        /// </summary>
+        /// <param name="desiredString">
+        /// An example string would be {0}, {1}, ...
+        /// </param>
+        public void SetOutputString(string desiredString)
+        {
+            outputString = desiredString;
+        }
+
+        /// <summary>
+        /// Method for creating the output file with the list of
+        /// subjects.
+        /// </summary>
+        /// <param name="directoryPath">
+        /// Path to write the file.
+        /// </param>
+        /// <param name="fileName">
+        /// Name of the file
+        /// </param>
+        /// <param name="SM">
+        /// Subject_Manager instance.
+        /// </param>
+        public void OutputToFile(string directoryPath, string fileName, Subject subjectToWrite)
+        {
+
+        }
+
+        #endregion
+    }
+}
