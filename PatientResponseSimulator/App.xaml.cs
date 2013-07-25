a1403fc7e56c3e844c876b361ff4ab246e44336e
abayeh <abayeh@gmail.com>
2013-07-25 14:21:43 -0400

    Initial setup

---
+++ PatientResponseSimulator/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿//------------------------------------------------------------------------------------------
+// <copyright file="App.xaml.cs" company="Tessella">
+//     Copyright © Tessella 2013. All rights reserved.
+// </copyright>
+// <project>Tessella C# training exercise, Assay Scheduler</project>
+// <summary></summary>
+// <svn>
+//  <lastChanged>$Date$</lastChanged>
+//  <by>BAYA</by>
+//  <version>$Revision$</version>
+//  <source>$HeadURL$</source>
+// </svn>
+//------------------------------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace PatientResponseSimulator
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
