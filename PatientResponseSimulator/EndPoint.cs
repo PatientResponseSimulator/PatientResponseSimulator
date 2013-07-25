ce2fe91cc9c49357fca7ff0fc6397050b0a3d136
titius <titius1@gmail.com>
2013-07-25 17:14:49 -0400

    New Classes

---
+++ PatientResponseSimulator/EndPoint.cs
@@ -0,0 +1,48 @@
+﻿//------------------------------------------------------------------------------------------
+// <copyright file=" EndPoint.cs" company="TESSELLA">
+//     Copyright © SET COPYRIGHT IN THE SNIPPET FILE. All rights reserved.
+// </copyright>
+// <project>Tessella/NPD/PatientResposeSimulator</project>
+// <summary></summary>
+// <svn>
+//  <lastChanged>$Date$</lastChanged>
+//  <by>$Author$</by>
+//  <version>$Revision$</version>
+//  <source>$HeadURL$</source>
+// </svn>
+//------------------------------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PatientResponseSimulator
+{
+    public enum EndpointType
+    {
+        Continous,
+        Dichotomous,
+        TimeToEvent
+    };
+
+    class Endpoint
+    {
+        #region Properties 
+        #endregion
+
+        #region Constructor
+        Endpoint()
+        {
+            Value = 0.0f;
+        }
+        #endregion
+
+
+        #region Methods
+        #endregion
+
+        double Value;
+        EndpointType Type;
+        string Name;
+    }
+}
---
+++ PatientResponseSimulator/Response.cs
@@ -0,0 +1,27 @@
+﻿//------------------------------------------------------------------------------------------
+// <copyright file=" GroupedSubject.cs" company="TESSELLA">
+//     Copyright © SET COPYRIGHT IN THE SNIPPET FILE. All rights reserved.
+// </copyright>
+// <project>Tessella/NPD/PatientResponseSimulator</project>
+// <summary></summary>
+// <svn>
+//  <lastChanged>$Date$</lastChanged>
+//  <by>Tiziano Diamanti</by>
+//  <version>$Revision$</version>
+//  <source>$HeadURL$</source>
+// </svn>
+//------------------------------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PatientResponseSimulator
+{
+    class Response
+    {
+        public
+        int VisiID;
+        List<Endpoint> Endpoints;
+    }
+}
